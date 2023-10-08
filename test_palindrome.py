9b40b0d4fa18b84e3707ee064559eaf6c28e9458
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2023-10-08 13:24:45 -0700

    Update test_palindrome.py

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,9 +1,21 @@
 import unittest
-
 from palindrome import *
 
-#test 1
+
 class test_palindrome(unittest.TestCase):
 
-    def test_basic(self):
-        self.assertEqual(isvalid(400), 798)
+# Middle case tests
+    def test_1(self):
+        self.assertTrue(isPalindrome("racecar") is True)
+    def test_2(self):
+        self.assertTrue(isPalindrome("panama") is False)
+    def test_3(self):
+        self.assertTrue(isPalindrome("A man, a plan, a canal: Panama") is True)
+    def test_4(self):
+        self.assertTrue(isPalindrome("Chunky Monkey") is False)
+
+# Edge case tests
+    def test_5(self):
+        self.assertTrue(isPalindrome("") is True)
+    def test_6(self):
+        self.assertTrue(isPalindrome("a") is True)
