8fb9e016563dbfa7f4c7a0e7fae54b0f63be7c1c
UsmanT12 <144747599+UsmanT12@users.noreply.github.com>
2023-10-10 11:46:03 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,22 @@
+'''
+Function to check if the input string is a palindrome 
+It is a palindrome if it is the same string when written backwards 
+All spaces, non alphanumeric characters, and character cases are ignored.
+'''
+
 def isPalindrome(s: str) -> bool:
-  s = s.lower()
-  string = ''
-  
-  for i in range(len(s)):
-      if s[i].isalnum() == True:
-          string += s[i]
-  begin = 0
-  end = len(string) - 1
-  
-  while (begin < end):
-      if string[begin] != string[end]:
-          return False
-      begin += 1
-      end -= 1
-  return True
+    s = s.lower()
+    string = ''
+
+    for i in range(len(s)):
+        if s[i].isalnum() == True:
+            string += s[i]
+    begin = 0
+    end = len(string) - 1
+
+    while begin < end:
+        if string[begin] != string[end]:
+            return False
+        begin += 1
+        end -= 1
+    return True
